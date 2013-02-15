a94d32a8f82b88aad833920b47200bea39db87d4
Paweł Kondzior <pawel@kondzior.com>
2013-02-15 21:41:12 +0100

    Propose some API organization

--- lib/bb_gun.rb
+++ lib/bb_gun.rb
@@ -1,13 +1,23 @@
+# encoding: UTF-8
+
 require "bb_gun/version"
 
 module BbGun
 
   class Converter
-    def initialize(options = {})
+    attr_reader :input, :options
+
+    def initialize(input, options = {})
+      @input = input
+      @options = options
     end
 
-    def convert(text)
-      text
+    def to_markdown
+      input
+    end
+
+    def self.to_markdown(input)
+      new(input).to_markdown
     end
   end
 end
