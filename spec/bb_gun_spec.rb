5e22c2e4eba00edf5246d1387856503f132b1db0
Paweł Kondzior <pawel@kondzior.com>
2013-02-15 21:29:25 +0100

    Wrap block in surrounding newlines

--- spec/bb_gun_spec.rb
+++ spec/bb_gun_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe BbGun do
+
   it "keeps plain text untouched" do
     converter = BbGun::Converter.new
     converter.convert("Some text").should == "Some text"
   end
+
 end
